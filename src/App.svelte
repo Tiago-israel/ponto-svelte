3eede0620b6c2cd3fd56f70dec94ffeb81824fd8
Tiago Rodrigues da Silva <tiagoi@braspag.com.br>
2022-01-04 23:20:30 -0300

    adição timer

--- src/App.svelte
+++ src/App.svelte
@@ -1,99 +1,169 @@
 <script>
-	let result = undefined;
-	const form = {
-		startHour: 0,
-		startMinutes: 0,
-		shortDay: false,
-		computeLunchBreak: true
-	};
-	const hours = Array(24).fill().map((_, index) => ({
-		value: index,
-		formatedValue: index < 10 ? `0${index}` : index
-	}));
-	const minutes = Array(60).fill().map((_, index) => ({
-		value: index,
-		formatedValue: index < 10 ? `0${index}` : index
-	}));
-	
+  let result = undefined;
+  let interval = undefined;
+  let countDown = "";
+  const form = {
+    startHour: 8,
+    startMinutes: 35,
+    shortDay: false,
+    computeLunchBreak: true,
+  };
+  const hours = Array(24)
+    .fill()
+    .map((_, index) => ({
+      value: index,
+      formatedValue: index < 10 ? `0${index}` : index,
+    }));
+  const minutes = Array(60)
+    .fill()
+    .map((_, index) => ({
+      value: index,
+      formatedValue: index < 10 ? `0${index}` : index,
+    }));
 
-	function handleSubmit(){
-		result = getWorkJourneyInfo(form);
-	}
+  function handleSubmit() {
+    result = getWorkJourneyInfo(form);
 
-	function getWorkJourneyInfo({ startHour = 0, startMinutes = 0, shortDay = false, computeLunchBreak = true })
-	{
-		if(startHour > 23 || startHour  < 0) throw new Error("error");
-		if(startMinutes > 59 || startMinutes < 0) throw new Error("error");
-		
-		const lunchBreakMinutes = 90;
-		
-		const shortDayHours = 6;
-		const normalDayHours = 8;
-		
-		const shortDayMinutes = 40;
-		const normalDayMinutes = 20;
-		
-		let start = new Date();
-		
-		start.setHours(0,0,0,0);
-		start.setHours(startHour, startMinutes);
-		let startTime = `${start.getHours()}:${start.getMinutes()}`
+	clearInterval(interval);
 
-		start.setMinutes(start.getMinutes() +  (computeLunchBreak ? lunchBreakMinutes : 0));
-		start.setHours( start.getHours() + (shortDay ? shortDayHours : normalDayHours));
-		start.setMinutes(start.getMinutes() + (shortDay ? shortDayMinutes : normalDayMinutes));
+    interval = setInterval(function () {
+      var firstDate = result.startDate.getTime();
+      var countDownDate = result.endDate.getTime();
 
-		let endTime = `${start.getHours()}:${start.getMinutes()}`;
+      var distance = countDownDate - firstDate;
 
-		return {startTime, endTime };
-	}
+      var hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      countDown = hours + "h " + minutes + "m " + seconds + "s ";
+
+      result.startDate.setSeconds(result.startDate.getSeconds() + 1);
+
+      if (distance < 0) {
+        clearInterval(interval);
+      }
+    }, 1000);
+  }
+
+  function getWorkJourneyInfo({
+    startHour = 0,
+    startMinutes = 0,
+    shortDay = false,
+    computeLunchBreak = true,
+  }) {
+    if (startHour > 23 || startHour < 0) throw new Error("error");
+    if (startMinutes > 59 || startMinutes < 0) throw new Error("error");
+
+    const lunchBreakMinutes = 90;
+
+    const shortDayHours = 6;
+    const normalDayHours = 8;
+
+    const shortDayMinutes = 40;
+    const normalDayMinutes = 20;
+
+    let start = new Date();
+
+    start.setHours(0, 0, 0, 0);
+    start.setHours(startHour, startMinutes);
+    let startTimeStr = `${start.getHours()}:${start.getMinutes()}`;
+    let startDate = new Date(start);
+
+    start.setMinutes(
+      start.getMinutes() + (computeLunchBreak ? lunchBreakMinutes : 0)
+    );
+    start.setHours(
+      start.getHours() + (shortDay ? shortDayHours : normalDayHours)
+    );
+    start.setMinutes(
+      start.getMinutes() + (shortDay ? shortDayMinutes : normalDayMinutes)
+    );
+
+    let endTimeStr = `${start.getHours()}:${start.getMinutes()}`;
+    let endDate = start;
+
+    return {
+      startTimeStr,
+      endTimeStr,
+      startDate,
+      endDate,
+    };
+  }
 </script>
 
 <main>
-	<h1 class="text-4xl text-center text-green-500 mb-5">Calcular horário de saída</h1>
-	<div class="main-container">
-		<form on:submit|preventDefault={handleSubmit}>
-			<div class="flex">
-				<select class="p-2 select w-100" bind:value="{form.startHour}">
-					{#each hours as hour}
-						<option value="{hour.value}">{hour.formatedValue}</option>
-					{/each}
-				</select>
-				<span class="mx-2">:</span>
-				<select class="select p-2 w-100" bind:value="{form.startMinutes}">
-					{#each minutes as minute}
-						<option value="{minute.value}">{minute.formatedValue}</option>
-					{/each}
-				</select>
-			</div>
-			<div class="flex items-center justify-between">
-				<div>
-					<div class="flex items-center gap-2">
-						<input type="checkbox"  bind:checked={form.shortDay}/>
-						<span>Sexta feira curta</span>
-					</div>
-					<div class="flex items-center gap-2">
-						<input type="checkbox"  bind:checked={form.computeLunchBreak}/>
-						<span>Adicionar horário de almoço (1h:30min)</span>
-					</div>
-				</div>
-				<button class="bg-green-500 text-white font-bold p-2">Calcular</button>
-			</div>
-		</form>
-		{#if result !== undefined}
-			<h1 class="text-3xl text-red-500 text-center mt-5">Horário saída {result.endTime}</h1>
-		{/if}
-	</div>
+  <h1 class="text-4xl text-center text-amber-400 mb-5">
+    Calcular horário de saída
+  </h1>
+  <div class="main-container">
+    <form on:submit|preventDefault={handleSubmit}>
+      <div class="flex">
+        <select class="p-2 select w-100" bind:value={form.startHour}>
+          {#each hours as hour}
+            <option value={hour.value}>{hour.formatedValue}</option>
+          {/each}
+        </select>
+        <span class="mx-2">:</span>
+        <select class="select p-2 w-100" bind:value={form.startMinutes}>
+          {#each minutes as minute}
+            <option value={minute.value}>{minute.formatedValue}</option>
+          {/each}
+        </select>
+      </div>
+      <div class="flex items-center justify-between">
+        <div>
+          <div class="flex items-center gap-2">
+            <input type="checkbox" bind:checked={form.shortDay} />
+            <span>Sexta feira curta</span>
+          </div>
+          <div class="flex items-center gap-2">
+            <input type="checkbox" bind:checked={form.computeLunchBreak} />
+            <span>Adicionar horário de almoço (1h:30min)</span>
+          </div>
+        </div>
+        <button class="bg-amber-400 text-white font-bold p-2">Calcular</button>
+      </div>
+    </form>
+    {#if result !== undefined}
+      <h1 class="text-3xl  text-amber-400 text-center mt-5">
+        Horário saída {result.endTimeStr}
+      </h1>
+	  <div class="w-100 flex items-center justify-center mt-5">
+		<div class="clock-container bg-amber-400 flex items-center justify-center">
+		  <div class="clock-content bg-amber-200 flex flex-col items-center justify-center">
+			<h1 class="text-3xl font-bold text-white">{countDown}</h1>
+			<span class="text-white font-bold">Tempo restante</span>
+		  </div>
+		</div>
+	  </div>
+    {/if}
+    
+  </div>
 </main>
 
 <style>
-	.w-100 {
-		width: 100%;
-	}
-	
-	.main-container {
-		width: 450px;
-		margin: 0 auto;
-	}
+  .w-100 {
+    width: 100%;
+  }
 
+  .main-container {
+    width: 450px;
+    margin: 0 auto;
+  }
+
+  .clock-container {
+    width: 300px;
+    height: 300px;
+    border-radius: 50%;
+  }
+
+  .clock-content {
+    width: 270px;
+    height: 270px;
+	border: 10px solid #fff;
+    border-radius: 50%;
+  }
 </style>
